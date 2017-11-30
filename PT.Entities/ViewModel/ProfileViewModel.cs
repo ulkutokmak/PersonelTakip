0260a0104710b6c8d5db65acd97a2484c32963da
Ülkü Tokmak <ulkutokmak@gmail.com>
2017-11-30 16:03:15 +0300

    Making a user profile

---
+++ PT.Entities/ViewModel/ProfileViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT.Entities.ViewModel
+{
+    public class ProfileViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name="Ad")]
+        [StringLength(25)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Soyad")]
+        [StringLength(35)]
+        public string Surname { get; set; }
+
+        [Required]
+        [Display(Name = "Kullanıcı Adı")]
+        [StringLength(25)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(100,MinimumLength =5,ErrorMessage ="Şifeniz en az 5 karakter olmalıdır!")]
+        [Display(Name = "Eski Şifre")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Şifeniz en az 5 karakter olmalıdır!")]
+        [Display(Name = "Yeni Şifre")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Şifeniz en az 5 karakter olmalıdır!")]
+        [Display(Name = "Şifre Tekrar")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage ="Şifereler Uyuşmuyor!")]
+        public string NewPasswordConfirm { get; set; }
+    }
+}
